57cf3d9762634911488d2017cb3b2e20779e3664
Krishna Dubagunta <dubagunta.saikrishna@outlook.com>
2018-04-30 18:05:58 -0400

    finished code

--- Auth-GraphQL/client/components/Header.js
+++ Auth-GraphQL/client/components/Header.js
@@ -1,21 +1,53 @@
 import React, { Component } from "react";
 import { graphql } from "react-apollo";
+import { Link } from "react-router";
 
 import { getCurrentUser } from "../queries";
+import { logout } from "../mutation";
 
 class Header extends Component {
+	onLogout() {
+		this.props.mutate({
+			refetchQueries: [{ query: getCurrentUser }]
+		});
+	}
+
 	renderButtons(me) {
 		if (!me) {
-			return <div>Login</div>;
+			return (
+				<div>
+					<li>
+						<Link to="/signup">Sign up</Link>
+					</li>
+					<li>
+						<Link to="/login">Login</Link>
+					</li>
+				</div>
+			);
 		}
-		return <div>Logout</div>;
+		return (
+			<li>
+				<a onClick={this.onLogout.bind(this)}>Logout</a>
+			</li>
+		);
 	}
 
 	render() {
 		const { loading, me } = this.props.data;
 		console.log(loading, me);
-		return <div>{loading ? <div /> : this.renderButtons(me)}</div>;
+		return (
+			<nav>
+				<div className="nav-wrapper container">
+					<Link className="brand-logo left" to="/">
+						Home
+					</Link>
+					<ul className="right">
+						{loading ? <div /> : this.renderButtons(me)}
+					</ul>
+				</div>
+			</nav>
+		);
 	}
 }
 
-export default graphql(getCurrentUser)(Header);
+export default graphql(logout)(graphql(getCurrentUser)(Header));
--- Auth-GraphQL/client/components/Index.js
+++ Auth-GraphQL/client/components/Index.js
@@ -2,12 +2,20 @@ import React, { Component } from "react";
 import { Router, hashHistory, Route, IndexRoute } from "react-router";
 
 import App from "./App";
+import LoginForm from "./LoginForm";
+import SignUpForm from "./SignUpForm";
+import Dashboard from "./Dashboard";
+import requireAuth from "./requireAuth";
 
 class Index extends Component {
 	render() {
 		return (
 			<Router history={hashHistory}>
-				<Route path="/" component={App} />
+				<Route path="/" component={App}>
+					<Route path="login" component={LoginForm} />
+					<Route path="signup" component={SignUpForm} />
+					<Route path="dashboard" component={requireAuth(Dashboard)} />
+				</Route>
 			</Router>
 		);
 	}
